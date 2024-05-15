2cf60f70a526eaafbc471865306fedc2b8c874c1
xuany <>
2024-05-15 17:27:42 +0800

    feat: success

--- countries/src/App.tsx
+++ countries/src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useCountries, useToggle } from "./hooks/useCounties";
 
 function App() {
+  const { countries, mapUser, onFilter } = useCountries();
+  const { sets, toggle } = useToggle();
+
+  function onChange(v: ChangeEvent<HTMLSelectElement>) {
+    onFilter(v.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "flex", margin: "20px 0", gap: "20px" }}>
+        <div>Filter By Gender</div>
+        <select onChange={onChange}>
+          <option value={"all"}>all</option>
+          <option value={"male"}>male</option>
+          <option value={"female"}>female</option>
+        </select>
+      </div>
+      <div className="list">
+        {countries.map((contry) => (
+          <>
+            <div onClick={() => toggle(contry)}>
+              {contry} - {mapUser[contry]?.length} users
+            </div>
+            <ul
+              style={{
+                height: !!sets[contry] ? "auto" : "0px",
+              }}
+              key={contry}
+              className="list-country"
+            >
+              {mapUser[contry]?.map((user) => (
+                <li className="list-user">
+                  <div style={{ fontWeight: "bold", marginBottom: "4px" }}>
+                    {user.name.title} {user.name.first} {user.name.last}
+                  </div>
+                  <div>gender: {user.gender}</div>
+                  <div>city: {user.location.city}</div>
+                  <div>state: {user.location.state}</div>
+                  <div>registered: {user.registered.date}</div>
+                </li>
+              ))}
+            </ul>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ countries/src/hooks/useCounties.ts
@@ -0,0 +1,72 @@
+import { useRef, useState } from "react"
+
+
+type IUser = {
+	name: {
+		title: string,
+		first: string,
+		last: string
+	},
+	gender: 'male' | 'female' | 'all',
+	location: {
+		country: string,
+		state: string,
+		street: { number: number, name: string}
+		city: string
+	},
+	registered: {date: string , age: number}
+}
+
+
+export function useCountries() {
+	const mountRef = useRef<Boolean>(false)
+	const sortedUsers = useRef<IUser[]>([])
+
+	const [countries, setCountries] = useState<string[]>([]);
+	const [mapUser, setMapUser] = useState<Record<string, IUser[]>>({});
+
+
+	function processContries(users: IUser[], gender = 'all') {
+		const map = users.reduce((pre, it) => {
+			pre[it.location.country] = pre[it.location.country] || []
+			pre[it.location.country].push(it)
+			return pre;
+		}, {} as Record<string, IUser[]>)
+		const countries = Object.keys(map).toSorted((a,b) => map[b]?.length - map[a]?.length)
+
+		setCountries(countries)
+		setMapUser(map)
+	}
+
+	function onFilter(gender = 'all') {
+		const filterUsers = sortedUsers.current.filter(it => gender === 'all' ? true : it.gender === gender)
+		processContries(filterUsers)
+	}
+
+	function loadData() {
+		fetch('https://randomuser.me/api/?results=100').then(res => res.json()).then((res: { results: IUser[] }) => {
+			sortedUsers.current ˜ res.results.toSorted((a,b) => Date.parse(b.registered.date) - Date.parse(a.registered.date))
+			processContries(sortedUsers.current)
+		})
+	}
+	if(!mountRef.current) {
+		loadData()
+		mountRef.current = true
+	}
+
+	return { countries, mapUser, onFilter }
+
+}
+
+
+export function useToggle() {
+	const [sets, setSets] = useState<Record<string, any>>({})
+	function toggle(value: any) {
+		if(sets[value]) {
+			setSets({[value]: false})
+		}else {
+			setSets({[value]: true})
+		}
+	}
+	return { sets, toggle }
+}
